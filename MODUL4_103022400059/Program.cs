463f357a6ba1ef0144566cfaf1d53bebfd0c3591
Naufal Ariq <sagirisamaa@gmail.com>
2026-03-28 14:15:35 +0700

    Second Commit

---
+++ MODUL4_103022400059/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Kodepaket
+{
+    public enum Paket
+    {
+        Basic,Standard,Premium,Unlimited,Gaming,Streaming,Family,Business,Student,Traveler
+    }
+    public string GetKodePaket(Paket jenispaket)
+    {
+        string[] kodepaket = { "P201", "P202", "P203", "P204", "P205", "P206", "P207", "P208", "P209", "P210", };
+        return kodepaket[(int)jenispaket];
+    }
+}
+
+class program
+{
+    static void Main()
+    {
+        Kodepaket kodepaket = new Kodepaket();
+        Console.WriteLine(kodepaket.GetKodePaket(Kodepaket.Paket.Student));
+    }
+}
+
